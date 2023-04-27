b3f5d05862f2f9c6d1371eaef0192e841f417954
dataymeric <13287345+dataymeric@users.noreply.github.com>
2023-04-27 23:09:18 +0200

    fix: preds refactor: minor code refactor style: linting

--- rcnn/test.py
+++ rcnn/test.py
@@ -4,23 +4,24 @@ from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
 from sklearn.metrics import roc_auc_score, confusion_matrix
-import matplotlib.pyplot as plt
 
 # create a new instance of the AudioDataset class for the test set
-test_dataset = AudioDataset(csv_file='../data/BirdVoxDCASE20k_csvpublic.csv', audio_dir="../data/BirdVox-DCASE-20k/wav")
+test_dataset = AudioDataset(
+    csv_file="../data/BirdVoxDCASE20k_csvpublic.csv",
+    audio_dir="../data/BirdVox-DCASE-20k/wav",
+)
 
 # create a DataLoader object to iterate over the test dataset
-test_loader = DataLoader(test_dataset, batch_size=16, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
 
 # Load the trained model
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = CRNN()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = CRNN().to(device)
 model.load_state_dict(torch.load("best_model.pt"))
-model = model.to(device)
 
 # Evaluate the model on the validation set
 model.eval()
-val_predictions = []
+val_preds = []
 val_targets = []
 
 print("Evaluating...")
@@ -28,12 +29,13 @@ with torch.no_grad():
     for inputs, labels in tqdm(test_loader):
         inputs, labels = inputs.to(device), labels.to(device)
         outputs = model(inputs)
-        _, preds = torch.max(outputs, 1)
-
-        val_predictions.extend(preds.cpu().numpy())
+        preds = outputs.round()
+        val_preds.extend(preds.cpu().numpy())
         val_targets.extend(labels.cpu().numpy())
 
-val_auc = roc_auc_score(val_targets, val_predictions)
+# Metrics
+val_auc = roc_auc_score(val_targets, val_preds)
 print(f"=> Val AUC: {val_auc:.4f}")
 
-cm = confusion_matrix(val_targets, val_predictions)
+cm = confusion_matrix(val_targets, val_preds)
+print(cm)
