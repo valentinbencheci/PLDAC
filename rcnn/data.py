23ce3055e63536c77a57cd1fde4482e89d4cc5d5
dataymeric <13287345+dataymeric@users.noreply.github.com>
2023-04-27 22:46:34 +0200

    feat: slicing data, added datasets fix: train/val loaders, lengths

--- rcnn/data.py
+++ rcnn/data.py
@@ -2,13 +2,28 @@ import numpy as np
 import pandas as pd
 import librosa
 import torch
-from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler, ConcatDataset
+from torch.utils.data import (
+    Dataset,
+    DataLoader,
+    WeightedRandomSampler,
+    ConcatDataset,
+    random_split,
+)
 from spectrogram import extract_log_mel_energy_features
 
 
 class AudioDataset(Dataset):
-    def __init__(self, csv_file, audio_dir, input_channels=1, transform=extract_log_mel_energy_features):
+    def __init__(
+        self,
+        csv_file,
+        audio_dir,
+        input_channels=1,
+        transform=extract_log_mel_energy_features,
+        data_slice=None,
+    ):
         self.data = pd.read_csv(csv_file, delimiter=",", header=0)
+        if data_slice:
+            self.data = self.data.sample(frac=data_slice / 100).reset_index(drop=True)
         self.labels = list(set(self.data["hasbird"]))
         self.label_to_idx = {label: i for i, label in enumerate(self.labels)}
         self.idx_to_label = {i: label for i, label in enumerate(self.labels)}
@@ -34,64 +49,74 @@ class AudioDataset(Dataset):
         return features, label_idx
 
 
-def load_data(batch_size):
-    # Load the data
-    dataset = AudioDataset("../data/warblrb10k_public_metadata.csv", "../data/warblrb10k_public/wav")
-
+def load_data(dataset, batch_size, split_size):
     # Calculate class weights
-    class_counts = [len(dataset.data[dataset.data['hasbird'] == label]) for label in dataset.labels]
+    class_counts = [
+        len(dataset.data[dataset.data["hasbird"] == label]) for label in dataset.labels
+    ]
     class_weights = 1.0 / torch.tensor(class_counts, dtype=torch.float)
+
+    # Split the dataset into training and validation sets
+    train_size = int(split_size * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
     # Create weighted random sampler
-    targets = [dataset.label_to_idx[label] for label in dataset.data['hasbird']]
+    train_indices = train_dataset.indices
+    targets = [
+        dataset.label_to_idx[label]
+        for label in dataset.data.loc[train_indices, "hasbird"]
+    ]
     sample_weights = [class_weights[class_idx] for class_idx in targets]
-    sampler = WeightedRandomSampler(sample_weights, len(sample_weights), replacement=True)
-
-    # Split the dataset into training and validation sets
-    train_size = int(0.8 * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+    sampler = WeightedRandomSampler(
+        sample_weights, len(sample_weights), replacement=True
+    )
 
     # Create data loaders
-    train_loader = DataLoader(dataset, batch_size=batch_size, sampler=sampler)
-    val_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=sampler)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
     return train_dataset, train_loader, val_dataset, val_loader
 
 
-def load_merged_data(batch_size):
-    # Load the data
-    warblrb10k = AudioDataset("../data/warblrb10k_public_metadata.csv", "../data/warblrb10k_public/wav")
-    ff1010bird = AudioDataset("../data/ff1010bird_metadata.csv", "../data/ff1010bird/wav")
+def load_merged_data(batch_size, split_size):
+    warblrb10k_train, _, warblrb10k_test, _ = load_data(
+        AudioDataset(
+            csv_file="../data/warblrb10k_public_metadata.csv",
+            audio_dir="../data/warblrb10k_public/wav",
+            data_slice=50,
+        ),
+        batch_size,
+        split_size,
+    )
 
-    dataset = ConcatDataset([warblrb10k, ff1010bird])
+    ff1010bird_train, _, ff1010bird_test, _ = load_data(
+        AudioDataset(
+            csv_file="../data/ff1010bird_metadata.csv",
+            audio_dir="../data/ff1010bird/wav",
+            data_slice=50,
+        ),
+        batch_size,
+        split_size,
+    )
 
-    # Calculate the class weights based on the balanced dataset:
-    class_counts = [len(warblrb10k.data[warblrb10k.data["hasbird"] == label]) + len(ff1010bird.data[ff1010bird.data["hasbird"] == label]) for label in warblrb10k.labels]
-    class_weights = 1.0 / torch.tensor(class_counts, dtype=torch.float)
+    birdvox_train, _, birdvox_test, _ = load_data(
+        AudioDataset(
+            csv_file="../data/BirdVoxDCASE20k_csvpublic.csv",
+            audio_dir="../data/BirdVox-DCASE-20k/wav",
+            data_slice=50,
+        ),
+        batch_size,
+        split_size,
+    )
 
-    # Create a list of targets and sample weights for the weighted random sampler:
-    targets1 = [warblrb10k.label_to_idx[label] for label in warblrb10k.data["hasbird"]]
-    sample_weights1 = [class_weights[class_idx] for class_idx in targets1]
+    train_loader = DataLoader(
+        ConcatDataset([warblrb10k_train, ff1010bird_train, birdvox_train]),
+        batch_size=batch_size,
+    )
+    val_loader = DataLoader(
+        ConcatDataset([warblrb10k_test, ff1010bird_test, birdvox_test]),
+        batch_size=batch_size,
+    )
 
-    targets2 = [ff1010bird.label_to_idx[label] for label in ff1010bird.data["hasbird"]]
-    sample_weights2 = [class_weights[class_idx] for class_idx in targets2]
-
-    targets = targets1 + targets2
-    sample_weights = sample_weights1 + sample_weights2
-
-    # Create the weighted random sampler:
-    sampler = WeightedRandomSampler(sample_weights, len(sample_weights), replacement=True)
-
-    # Split the dataset into training and validation sets
-    train_size = int(0.8 * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
-
-    # Create data loaders
-    train_loader = DataLoader(dataset, batch_size=batch_size, sampler=sampler)
-    val_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-    return train_dataset, train_loader, val_dataset, val_loader
-
-
+    return train_loader, val_loader
